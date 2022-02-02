182d93228555d5f14ee3dc561ae1ab01d4b1ad68
Jędrzej Boczar <yendreij@gmail.com>
2022-02-02 20:06:03 +0100

    feat(half2half): initial Message for communication between halves

---
+++ src/half2half.rs
@@ -0,0 +1,49 @@
+use defmt::Format;
+use serde::{Serialize, Deserialize};
+use postcard::{flavors::{Slice, Cobs}, CobsAccumulator};
+use heapless::Vec;
+
+use crate::hal_ext::crc;
+
+#[derive(Serialize, Deserialize, Format)]
+pub enum Message {
+    EstablishMaster,
+    ReleaseMaster,
+    Ack,
+}
+
+
+
+impl Message {
+    pub fn to_slice<'a>(&self, crc: &mut crc::Crc, buf: &'a mut [u8]) -> postcard::Result<&'a mut [u8]> {
+        crc.serialize_to_slice_cobs::<Self>(self, buf)
+    }
+
+    pub fn to_vec<'a, const N: usize>(&self, crc: &mut crc::Crc) -> postcard::Result<Vec<u8, N>> {
+        crc.serialize_to_vec_cobs::<Self, N>(self)
+    }
+
+    // TODO: maybe try to somehow return `impl Iterator<Item=Message>`
+    pub fn from_slice<F, const N: usize>(
+        cobs_acc: &mut CobsAccumulator<N>,
+        data: &[u8],
+        mut callback: F,
+    )
+    where
+        F: FnMut(Message)
+    {
+        use postcard::FeedResult::*;
+        let mut window = data;
+        while !window.is_empty() {
+            window = match cobs_acc.feed::<Self>(&window) {
+                Consumed => break,
+                OverFull(new_window) => new_window,
+                DeserError(new_window) => new_window,
+                Success { data, remaining } => {
+                    callback(data);
+                    remaining
+                }
+            }
+        }
+    }
+}
